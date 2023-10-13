925adee471c821f026d531f9645e03960b852938
Xavier Cotton <112589714+Eldarch@users.noreply.github.com>
2023-10-13 12:35:06 -0600

    Update my_python_node.py

--- ros2_ws/my_python_pkg/my_python_pkg/my_python_node.py
+++ ros2_ws/my_python_pkg/my_python_pkg/my_python_node.py
@@ -3,4 +3,15 @@ from rclpy.node import Node
 
 class MyPythonNode(Node):
     def __init__(self):
-        super
+        super().__init__("my_node_name")
+        self.get_logger().info("This node just says 'Hello'")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyPythonNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
