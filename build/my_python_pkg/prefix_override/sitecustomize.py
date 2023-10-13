6afb0a7b773ab9eff228a9cc959983888b75ffe3
Xavier <xcotton@mines.edu>
2023-10-13 18:33:20 +0000

    python pkg

---
+++ build/my_python_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/mines/mines_ws/captureData/scrap/install/my_python_pkg'
---
+++ ros2_ws/build/my_python_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/mines/mines_ws/captureData/scrap/ros2_ws/install/my_python_pkg'
